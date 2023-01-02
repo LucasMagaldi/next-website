3d491b3328482d1a7587e9ddd8f8bdd877bca443
LucasMagaldi <lucas.magaldi@hotmail.com>
2023-01-02 00:06:48 -0300

    toggle header items

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const menu = document.querySelector("#mobile_menu");
-const bar = document.querySelectorAll("bar");
+const menuLinks = document.querySelector(".header_menu");
 
 menu.addEventListener("click", () => {
+    console.log(menuLinks)
     menu.classList.toggle("is_active");
+    menuLinks.classList.toggle("active");
 })
